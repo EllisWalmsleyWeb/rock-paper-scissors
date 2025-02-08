d9665634d8e8fd18e315cfb194c42e8fdc637c76
elliswalmsleyweb <elliswalmsleyweb@gmail.com>
2025-02-08 23:46:36 +0000

    getHumanChoice function created

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ let getComputerChoice = (choice) => {
 };
 
 console.log("The computer chose " + getComputerChoice() + ".");
+
+let getHumanChoice = () => {
+  let choice = prompt("rock, paper, or scissors?").toLowerCase();
+
+  if (choice === "rock" || choice === "paper" || choice === "scissors") {
+    return choice; // Valid input, return it
+  }
+
+  alert("Invalid choice. Please enter rock, paper, or scissors.");
+  return getHumanChoice(); // Recursively call the function again
+};
+
+console.log("You chose " + getHumanChoice() + ".");
