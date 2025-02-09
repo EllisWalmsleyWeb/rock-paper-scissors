b0f2d5b7ee6611560e6d4ce6f436aa4e5e10fd00
elliswalmsleyweb <elliswalmsleyweb@gmail.com>
2025-02-09 00:02:53 +0000

    playRound function created to play a single round.

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 let humanScore = 0;
 let computerScore = 0;
 
-let getComputerChoice = (choice) => {
+let getComputerChoice = () => {
   choice = Math.floor(Math.random() * 3);
   if (choice == 0) {
     return "rock";
   } else if (choice == 1) {
-    return "scissors";
-  } else if (choice == 2) {
     return "paper";
+  } else if (choice == 2) {
+    return "scissors";
   }
 };
-
-console.log("The computer chose " + getComputerChoice() + ".");
 
 let getHumanChoice = () => {
   let choice = prompt("rock, paper, or scissors?").toLowerCase();
@@ -25,4 +23,25 @@ let getHumanChoice = () => {
   return getHumanChoice(); // Recursively call the function again
 };
 
-console.log("You chose " + getHumanChoice() + ".");
+function playRound(humanChoice, computerChoice) {
+  if (
+    (humanChoice === "rock" && computerChoice === "paper") ||
+    (humanChoice === "paper" && computerChoice === "scissors") ||
+    (humanChoice === "scissors" && computerChoice === "rock")
+  ) {
+    computerScore++;
+    return `You lose! ${computerChoice} beats ${humanChoice}`;
+  } else if (humanChoice === computerChoice) {
+    return `It's a tie! You both picked ${computerChoice}`;
+  } else {
+    humanScore++;
+    return `You win! ${humanChoice} beats ${computerChoice}`;
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
+console.log("Computer score: " + computerScore);
+console.log("Human score: " + humanScore);
